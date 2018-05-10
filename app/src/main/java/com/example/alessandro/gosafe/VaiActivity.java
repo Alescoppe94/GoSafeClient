d9195b364423a8d351773a3f3daa3df3c1833105
lucalustrissimini <luca.lst@virgilio.it>
2018-05-10 16:30:52 +0200

    Aggiunta mappa 145, eliminate 150 155 e 160, sistemato vai e mappe

--- app/src/main/java/com/example/alessandro/gosafe/VaiActivity.java
+++ app/src/main/java/com/example/alessandro/gosafe/VaiActivity.java
@@ -2,6 +2,8 @@ package com.example.alessandro.gosafe;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
@@ -11,8 +13,11 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,70 +25,115 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.example.alessandro.gosafe.beacon.BluetoothLeService;
 import com.example.alessandro.gosafe.database.DAOUtente;
 import com.example.alessandro.gosafe.entity.Utente;
+import com.example.alessandro.gosafe.helpers.PinView;
 import com.example.alessandro.gosafe.server.CheckForDbUpdatesService;
 
 public class VaiActivity extends DefaultActivity {
 
-    Spinner spinnerVai;
+    private ScaleGestureDetector SGD;
+    private Matrix matrix = new Matrix();
+    private ImageView imageView;
+    private Float scale =1f;
+    private PointF newCoord;
+    private boolean load = true;
+
+    /*roba per menu a tendina*/
+    Spinner spinner;
+    private PinView pinView;
+    int x;
+    int y;
+
     ArrayAdapter<CharSequence> adapter;
-    ImageView imageViewVai;
+    private PinView imageViewPiano;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vai);
+        setContentView(R.layout.activity_mappe);
 
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter != null) {
-            DAOUtente daoUtente = new DAOUtente(this);
-            daoUtente.open();
-            Utente user = daoUtente.findUtente();
-            daoUtente.close();
-            Intent s = new Intent(this, BluetoothLeService.class);            //rimanda l'utente al servizio, può essere modificato
-            Bundle bundle = new Bundle();
-            bundle.putSerializable("user", user);
-            bundle.putLong("periodo", 20000);
-            s.putExtras(bundle);
-            startService(s);
-        }
 
-        Intent u = new Intent(this, CheckForDbUpdatesService.class);
-        startService(u);
-
-        //Spinner
-        spinnerVai= (Spinner) findViewById(R.id.spinnerVai);
+        spinner=(Spinner)findViewById(R.id.spinner);
         adapter=ArrayAdapter.createFromResource(this,R.array.piani,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerVai.setAdapter(adapter);
-        imageViewVai = (ImageView) findViewById(R.id.imageViewPianoVai);
-        spinnerVai.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinner.setAdapter(adapter);
+
+        imageViewPiano = (PinView) findViewById(R.id.imageViewPiano);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                int position = spinnerVai.getSelectedItemPosition();
-                Toast.makeText(getBaseContext(),adapterView.getItemAtPosition(i)+" selected",Toast.LENGTH_LONG).show();
-                switch(position){
+                int position = spinner.getSelectedItemPosition();
+                Toast.makeText(getBaseContext(), adapterView.getItemAtPosition(i) + " selected", Toast.LENGTH_LONG).show();
+                switch (position) {
                     case 0:
-                        imageViewVai.setImageResource(R.drawable.q140);
+                        imageViewPiano.setImage(ImageSource.resource(R.drawable.q140));
+                        load = true;
                         break;
                     case 1:
-                        imageViewVai.setImageResource(R.drawable.quota145);
-                        break;
-                    case 2:
-                        imageViewVai.setImageResource(R.drawable.quota150);
-                        break;
-                    case 3:
-                        imageViewVai.setImageResource(R.drawable.quota155);
+                        imageViewPiano.setImage(ImageSource.resource(R.drawable.q145));
+                        load = true;
                         break;
                 }
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
+
+        //serve per definire le gesture da rilevare, per ora lo uso solo per settare il pin con long press
+        final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                if (imageViewPiano.isReady()) {
+                    PointF sCoord = imageViewPiano.viewToSourceCoord(e.getX(), e.getY());
+                    Toast.makeText(getApplicationContext(), "Single tap: " + ((int)sCoord.x) + ", " + ((int)sCoord.y), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Single tap: Image not ready", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+            @Override
+            public void onLongPress(MotionEvent e) {
+                if (imageViewPiano.isReady()) {
+                    PointF sCoord = imageViewPiano.viewToSourceCoord(e.getX(), e.getY());
+                    if(load) {
+                        /*Permette di capire quali sono i corrispettivi su schermo dei veri punti della mappa*/
+                        PointF mCoord = imageViewPiano.sourceToViewCoord((float) 346 , (float) 1072);
+                        newCoord = imageViewPiano.viewToSourceCoord(mCoord.x,mCoord.y);
+                        load = false;
+                    }
+                    imageViewPiano.play(sCoord, newCoord);
+                    Toast.makeText(getApplicationContext(), "Long press: " + ((int)sCoord.x) + ", " + ((int)sCoord.y), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Long press: Image not ready", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                if (imageViewPiano.isReady()) {
+                    PointF sCoord = imageViewPiano.viewToSourceCoord(e.getX(), e.getY());
+                    Toast.makeText(getApplicationContext(), "Double tap: " + ((int)sCoord.x) + ", " + ((int)sCoord.y), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Double tap: Image not ready", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+        //setto il listener per l'evento
+        imageViewPiano.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return gestureDetector.onTouchEvent(motionEvent);
+            }
+        });
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
