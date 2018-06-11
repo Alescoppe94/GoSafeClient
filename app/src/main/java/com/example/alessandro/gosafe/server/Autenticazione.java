cd790299909c969609787d62079234677d600cc9
DESKTOP-5I1TOUH\Alessandro <scoppe28994@gmail.com>
2018-06-11 18:30:35 +0200

    Merge branch 'Fusione' of https://github.com/Alescoppe94/GoSafeClient into Fusione
    # Conflicts:
    #	app/src/main/java/com/example/alessandro/gosafe/server/AggiornamentoInfoServer.java
    #	app/src/main/java/com/example/alessandro/gosafe/server/Autenticazione.java
    #	app/src/main/java/com/example/alessandro/gosafe/server/CheckConnessione.java
    #	app/src/main/java/com/example/alessandro/gosafe/server/CheckForDbUpdatesService.java
    #	app/src/main/java/com/example/alessandro/gosafe/server/DbDownloadFirstBoot.java
    #	app/src/main/java/com/example/alessandro/gosafe/server/RichiestaPercorso.java

--- app/src/main/java/com/example/alessandro/gosafe/server/Autenticazione.java
+++ app/src/main/java/com/example/alessandro/gosafe/server/Autenticazione.java
@@ -55,7 +55,7 @@ public class Autenticazione {
     private UserSessionManager session;
     private Utente utente_attivo;
     private HttpURLConnection connection;
-    private final String PATH = "http://192.168.1.197:8080";
+    private final String PATH = "http://10.0.2.2:8080";
 
     public Autenticazione(Utente utente_attivo) {
         this.utente_attivo = utente_attivo;
--- app/src/main/java/com/example/alessandro/gosafe/server/RichiestaPercorso.java
+++ app/src/main/java/com/example/alessandro/gosafe/server/RichiestaPercorso.java
@@ -28,7 +28,7 @@ import java.util.concurrent.ExecutionException;
 public class RichiestaPercorso {
 
     private HttpURLConnection conn;
-    private final String PATH = "http://192.168.1.197:8080";
+    private final String PATH = "http://10.0.2.2:8080";
     private Utente utente_attivo;
     public Percorso percorsoPost;
     public Percorso percorsoEmergenza;
