80d301bd17dcc1f0f0eb01d0402d8aa90292e5e1
DESKTOP-5I1TOUH\Alessandro <scoppe28994@gmail.com>
2018-07-13 22:13:02 +0200

    commenti

--- app/src/main/java/com/example/alessandro/gosafe/entity/Notifica.java
+++ app/src/main/java/com/example/alessandro/gosafe/entity/Notifica.java
@@ -1,5 +1,8 @@
 package com.example.alessandro.gosafe.entity;
 
+/**
+ * classe che modella l'entity Notifica
+ */
 public class Notifica {
 
     private int id;
@@ -7,20 +10,38 @@ public class Notifica {
     private Percorso percorso;
     private String messaggio;
 
+    /**
+     * costruttore
+     * @param utenteId id dell'utente a cui è rivolta la notifica
+     * @param percorso percorso ricevuto dal server
+     * @param messaggio messaggio ricevuto sal server
+     */
     public Notifica(int utenteId, Percorso percorso, String messaggio) {
         this.utenteId = utenteId;
         this.percorso = percorso;
         this.messaggio = messaggio;
     }
 
+    /**
+     * metodo getter che recupera l'id della notifica
+     * @return ritorna l'id della notifica
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * metodo setter che imposta l'id della notifica
+     * @param id prende l'id della notifica
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * metodo getter che recupera il percorso ricevuto dal server
+     * @return ritorna il percorso. e' il percorso di evacuazione
+     */
     public Percorso getPercorso() {
         return percorso;
     }
