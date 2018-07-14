94c6f7651415f83047f6babd7a2378467ae60c9f
Alessandro <scoppe28994@gmail.com>
2018-07-14 14:38:18 +0200

    rimosse vari

--- app/src/main/java/com/example/alessandro/gosafe/server/RichiestaPercorso.java
+++ app/src/main/java/com/example/alessandro/gosafe/server/RichiestaPercorso.java
@@ -58,6 +58,10 @@ public class RichiestaPercorso {
         this.utente_attivo = utente_attivo;
     }
 
+    /**
+     * metodo che ritorna il risultato dell'operazione
+     * @return ritorna il risultato dell'AsyncTask
+     */
     public String getResult(){
         try {
             return task.get();
@@ -85,24 +89,39 @@ public class RichiestaPercorso {
         return percorsoEmer;
     }
 
+    /**
+     * metodo che si occupa di avviare la procedura di cambio piano
+     * @param imageViewPiano immagine del piano
+     * @param posizione numero del piano che si vuole impostare
+     */
     public void cambiaPiano(PinView imageViewPiano, int posizione) {
         new CambiaPianoTask(imageViewPiano, posizione);
     }
 
+    /**
+     * classe che gestisce il disegno del percorso su più piani
+     */
     private class CambiaPianoTask {
 
 
         private PinView imageViewPiano;
         private int posizione;
 
+        /**
+         * costruttore
+         * @param imageViewPiano contiene l'immagine del piano
+         * @param posizione contiene il numero del piano su cui ci trova
+         */
         public CambiaPianoTask(PinView imageViewPiano, int posizione){
             this.imageViewPiano = imageViewPiano;
             this.posizione = posizione;
-            disegnaPercorso();
+            disegnaPercorso(); //disegna il percorso su più piani
         }
 
 
-
+        /**
+         * disegna il percorso su più piani
+         */
         protected void disegnaPercorso() {
 
             coorddelpercorso.clear();
@@ -113,7 +132,7 @@ public class RichiestaPercorso {
                     coorddelpercorso.add(percorso.get(j+1));
                 }
             }
-            imageViewPiano.play(coorddelpercorso);
+            imageViewPiano.play(coorddelpercorso); //metodo che disegna il percorso vero e proprio
 
         }
     }
@@ -146,6 +165,14 @@ public class RichiestaPercorso {
         private Spinner spinner;
         private ProgressDialog calcolopercorso_in_corso;
 
+        /**
+         * costruttore
+         * @param ctx rappresenta il context dell'applicazione
+         * @param beaconArr rappresenta il beacon di destinazione
+         * @param imageViewPiano rappresenta la mappa di partenza
+         * @param posizione rappresenta la posizione di partenza
+         * @param spinner rappresenta il menù a tendina delle mappe
+         */
         public OttieniPercorsoNoEmergenzaTask(Context ctx, String beaconArr, PinView imageViewPiano, int posizione, Spinner spinner){
             this.ctx = ctx;
             this.beaconArr = beaconArr;
@@ -155,7 +182,7 @@ public class RichiestaPercorso {
         }
 
         /**
-         * Verifica la connessione con il server
+         * Verifica la connessione con il server. parte prima dell'AsyncTask vero e proprio
          */
         @Override
         protected void onPreExecute() {
@@ -172,9 +199,9 @@ public class RichiestaPercorso {
         }
 
         /**
-         * Effettua la richiesta del percorso al server
-         * @param voids
-         * @return
+         * Effettua la richiesta del percorso al server. E' l'AsyncTask vero e proprio. Effettua una chiamata GET
+         * @param voids è un parametro vuoto
+         * @return ritorna il risultato dell'operazione
          */
         @Override
         protected String doInBackground(Void... voids) {
@@ -188,13 +215,15 @@ public class RichiestaPercorso {
                     System.out.println("BEACON DI ARRIVO IN RICH PERC: " +beaconArr);
                     SharedPreferences prefs = ctx.getSharedPreferences("ipAddress", MODE_PRIVATE);
                     String path = prefs.getString("ipAddress", null);
-                    URL url = new URL("http://" + path + "/gestionemappe/mappe/secured/calcolapercorso/"+utente_attivo.getBeaconid()+"/"+beaconArr+""); //Gli devo passare il beacon d'arrivo
+                    URL url = new URL("http://" + path + "/gestionemappe/mappe/secured/calcolapercorso/"+utente_attivo.getBeaconid()+"/"+beaconArr+""); //Gli devo passare il beacon d'arrivo. è l'url della chiamata REST
                     conn = (HttpURLConnection) url.openConnection();
+                    //imposta l'header della richiesta
                     conn.setDoInput(true);
                     conn.setRequestMethod("GET");
                     conn.setRequestProperty("Authorization", "basic " + base64);
                     conn.connect();
 
+                    //controlla il codice di risposta
                     int responseCode = conn.getResponseCode();
                     if(400 <= responseCode && responseCode <= 499){
                         calcolopercorso_in_corso.dismiss();
@@ -205,6 +234,7 @@ public class RichiestaPercorso {
                     BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
                     String inputLine;
 
+                    //scrive la risposta su una stringa
                     while ((inputLine = br.readLine()) != null) {
                         sb.append(inputLine + "\n");
                     }
@@ -226,12 +256,20 @@ public class RichiestaPercorso {
             return null;
         }
 
+        /**
+         * metodo che in questo caso non viene utilizzato
+         * @param arg0 è un parametro vuoto
+         */
         @Override
         protected void onProgressUpdate(Void... arg0){
 
         }
 
 
+        /**
+         * metodo che analizza la risposta del server contenente il percorso
+         * @param result
+         */
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
@@ -297,23 +335,36 @@ public class RichiestaPercorso {
     }
 
     /**
-     * Classe che visualizza il percorso
+     * metodo che avvia l'AsyncTask che visualizza il percorso in caso di emergenza. vien lanciato ogni volta
+     * che cambia il beacon a cui si è connessi poichè le condizioni possono cambiare
      * @param ctx contesto dell'applicazione
      * @param imageViewPiano mappa del piano di partenza del percorso
      */
     public void visualizzaPercorso(Context ctx, PinView imageViewPiano) { task = new VisualizzaPercorsoTask(ctx, imageViewPiano).execute(); }
 
+    /**
+     * classe che modella l'AsyncTask per visualizzare il percorso in caso di emergenza
+     */
     private class VisualizzaPercorsoTask extends AsyncTask<Void,Void,String> {
         private Context ctx;
         private boolean connesso;
         private AsyncTask<Void, Void, Boolean> execute;
         private PinView imageViewPiano;
 
+        /**
+         * costruttore
+         * @param ctx contesto dell'applicazione
+         * @param imageViewPiano mappa del piano di partenza del percorso
+         */
         public VisualizzaPercorsoTask(Context ctx, PinView imageViewPiano) {
             this.ctx = ctx;
             this.imageViewPiano = imageViewPiano;
         }
 
+        /**
+         * metodo che viene eseguito prima prima dell'AsyncTask vero e proprio.
+         * verifica la connessione al serve
+         */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -321,6 +372,12 @@ public class RichiestaPercorso {
             connesso = checkConnessione.checkConnessione(ctx);
         }
 
+        /**
+         * metodo che rappresenta l'AsyncTask vero e proprio. si occupa di fare una richiesta GET al server per ottenere
+         * il percorso di evacuazione
+         * @param voids è un parametro vuoto
+         * @return ritorna il risultato del server
+         */
         @Override
         protected String doInBackground(Void... voids) {
 
@@ -332,13 +389,15 @@ public class RichiestaPercorso {
                     String base64 = android.util.Base64.encodeToString(data, Base64.DEFAULT);
                     SharedPreferences prefs = ctx.getSharedPreferences("ipAddress", MODE_PRIVATE);
                     String path = prefs.getString("ipAddress", null);
-                    URL url = new URL("http://" + path + "/gestionemappe/mappe/secured/visualizzapercorso/" + utente_attivo.getId_utente() + "/" + utente_attivo.getBeaconid());
+                    URL url = new URL("http://" + path + "/gestionemappe/mappe/secured/visualizzapercorso/" + utente_attivo.getId_utente() + "/" + utente_attivo.getBeaconid()); //url a cui si fa la richiesta. prende l'id dell'utente e la posizione di quest'ultimo
                     conn = (HttpURLConnection) url.openConnection();
+                    //imposta l'header della richiesta
                     conn.setDoInput(true);
                     conn.setRequestMethod("GET");
                     conn.setRequestProperty("Authorization", "basic " + base64);
                     conn.connect();
 
+                    //controlla il codice della risposta
                     int responseCode = conn.getResponseCode();
                     if(400 <= responseCode && responseCode <= 499){
                         this.cancel(true);
@@ -348,6 +407,7 @@ public class RichiestaPercorso {
                     BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
                     String inputLine;
 
+                    //scrive il risultato in una stringa
                     while ((inputLine = br.readLine()) != null) {
                         sb.append(inputLine + "\n");
                     }
@@ -369,23 +429,28 @@ public class RichiestaPercorso {
             return null;
         }
 
+        /**
+         * metodo eseguito subito dopo doInBackground. è un metodo di raccordo tra AsyncTask e Main Thread.
+         * analizza il risultato del server. Se il server è offline calcola il percorso localmente altrimenti ogni volta estrae il percorso
+         * calcolato dalla notifica inviata dal server.
+         * @param result
+         */
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            if (result == null) {
+            if (result == null) { // se il server è offline calcola il percorso localmente
                 percorsoEmergenza = calcolaPercorsoEmergenza(utente_attivo.getBeaconid(),ctx);
-            } else {
+            } else { //altrimenti si estrae il percorso dalla notifica proveniente dal server sotto forma di json
                 Notifica notifica = new Gson().fromJson(result, Notifica.class);
                 percorsoEmergenza = notifica.getPercorso();
             }
-            // Devo tradurre il Percorso percorsoEmergenza in Tappe poi in Tronchi poi in ArrayList<Integer> percorso
-            //1 8 8 4 4 5
+            // Si traduce il Percorso percorsoEmergenza in Tappe poi in Tronchi poi in ArrayList<Integer> percorso che rappresenta la sequenza di beacon
             percorsoEmer.clear();
             percorsoEmer.add(percorsoEmergenza.getTappe().get(0).getTronco().getBeaconEstremi().get(0).getId());
             DAOBeacon daoBeacon = new DAOBeacon(ctx);
             daoBeacon.open();
             int pianoId = daoBeacon.getBeaconById(utente_attivo.getBeaconid()).getPiano();
-            for (int i = 1; i < percorsoEmergenza.getTappe().size() - 1; i++) { //1 8 8 4 4
+            for (int i = 1; i < percorsoEmergenza.getTappe().size() - 1; i++) {
                 Tappa tappa = percorsoEmergenza.getTappe().get(i);
                 Beacon beacon = tappa.getTronco().getBeaconEstremi().get(0);
                 if(beacon.getPiano() == pianoId) {
@@ -395,21 +460,29 @@ public class RichiestaPercorso {
             Beacon penultimoBeacon = percorsoEmergenza.getTappe().get(percorsoEmergenza.getTappe().size() - 1).getTronco().getBeaconEstremi().get(0);
             Beacon ultimoBeacon = percorsoEmergenza.getTappe().get(percorsoEmergenza.getTappe().size() - 1).getTronco().getBeaconEstremi().get(1);
             if(penultimoBeacon.getPiano() == pianoId) {
-                percorsoEmer.add(penultimoBeacon.getId());            //1 8 8 4 4 5 ->
+                percorsoEmer.add(penultimoBeacon.getId());
             }
+            //controlla se un percorso è su più piani
             boolean percorsoConPiuPiani = false;
             if(ultimoBeacon.getPiano() == pianoId) {
-                percorsoEmer.add(ultimoBeacon.getId());            //1 8 8 4 4 5 ->
+                percorsoEmer.add(ultimoBeacon.getId());
             }
             else percorsoConPiuPiani = true;
 
             coordEmergenza = daoBeacon.getCoords(percorsoEmer);  // Crea una lista in cui vengono contenuti le coordinate di tutti i beacon del percorso
             imageViewPiano.setPercorsoConPiuPiani(percorsoConPiuPiani);
-            imageViewPiano.play(coordEmergenza);
+            imageViewPiano.play(coordEmergenza); // viene disegnato il percorso
             daoBeacon.close();
         }
     }
 
+    /**
+     * metodo che si occupa di calcolare localmente il percorso in fase di Non Emergenza qualora il server
+     * fosse offline. Fa utilizzo delle informazioni presenti sul db locale
+     * @param ctx context dell'applicazione
+     * @param beaconArr beacon di arrivo del percorso
+     * @return ritorna il percorso calcolato
+     */
     public Percorso calcolaPercorsoNoEmergenza(Context ctx, String beaconArr) {
         boolean emergenza = false;
         DAOBeacon beaconDAO = new DAOBeacon(ctx);
@@ -419,9 +492,10 @@ public class RichiestaPercorso {
         beaconDAO.close();
         Percorso percorso;
 
+        //se nè l'arrivo nè la partenza sono nulle allora il percorso viene calcolato
         if (partenza != null && arrivo != null) {
 
-            Map<LinkedList<Beacon>, Float> percorsoOttimo_costoOttimo =  calcoloDijkstra(partenza, arrivo, emergenza, ctx);
+            Map<LinkedList<Beacon>, Float> percorsoOttimo_costoOttimo =  calcoloDijkstra(partenza, arrivo, emergenza, ctx);  //si chiama il metodo che applica l'algoritmo di Djikstra
 
             LinkedList<Tappa> tappeOttime = new LinkedList<>();
 
@@ -443,6 +517,14 @@ public class RichiestaPercorso {
         return percorso;
     }
 
+    /**
+     * metodo che si occupa di calcolare localmente il percorso in fase di Emergenza qualora il server
+     * fosse offline. Fa utilizzo delle informazioni presenti sul db locale. La destinazione è calcolata in base al punto
+     * di raccolta che minimizza la funzione di calcolo del costo.
+     * @param beaconPart beacon di partenza
+     * @param ctx context dell'applicazion
+     * @return ritorna il percorso
+     */
     public Percorso calcolaPercorsoEmergenza(String beaconPart, Context ctx) {
         DAOBeacon beaconDAO = new DAOBeacon(ctx);
         beaconDAO.open();
@@ -492,6 +574,18 @@ public class RichiestaPercorso {
         return null;
     }
 
+    /**
+     * metodo che si occupa di applicare l'algoritmo di Djikstra al calcolo del percorso sia in fase di
+     * emergenza che in fase di non emergenza. Ritorna il percorso ottimo verso la destinazione.
+     * In base a se è una fase di Emergenza o meno utilizza due formule diverse per calcolare il costo:
+     * se c'è un'emergenza tiene conto di tutti i parametri (affollamento, fumo, ecc.) mentre in fase di emergenza
+     * solo della lunghezza dei tronchi.
+     * @param partenza rappresenta il beacon di partenza
+     * @param arrivo rappresenta il beacon di arrivo
+     * @param emergenza rappresenta se siamo in una situazione di emergenza o meno
+     * @param ctx context dell'applicazione
+     * @return ritorn il percorso ottimo
+     */
     private Map<LinkedList<Beacon>, Float> calcoloDijkstra(Beacon partenza, Beacon arrivo, boolean emergenza, Context ctx){
 
         DAOTronco troncoDAO = new DAOTronco(ctx);
@@ -539,11 +633,11 @@ public class RichiestaPercorso {
                 }
                 Float costo;
                 if (emergenza){
-                    costo = tronco.calcolaCosto(ctx);
-                }else{
+                    costo = tronco.calcolaCosto(ctx); // in fase di emergenza utilizza un metodo che tiene conto di tutti i parametri
+                }else{ //in fase di non emergenza considera solo la lunghezza
                     DAOPesiTronco pesiTroncoDAO = new DAOPesiTronco(ctx);
                     pesiTroncoDAO.open();
-                    costo = pesiTroncoDAO.geValoreByPesoId(tronco.getId(), "l");
+                    costo = pesiTroncoDAO.geValoreByPesoId(tronco.getId(), "l"); // la lunghezza corrisponde al peso di id 1
                     pesiTroncoDAO.close();
                 }
                 costo_percorso_parziale += costo;
@@ -595,6 +689,12 @@ public class RichiestaPercorso {
 
     }
 
+    /**
+     * controlla se un beacon è contenuto in unArrayList di beacons
+     * @param beacons
+     * @param beacon
+     * @return
+     */
     private boolean compare(ArrayList<Beacon> beacons, Beacon beacon){
 
         boolean contenuto = false;
@@ -609,6 +709,12 @@ public class RichiestaPercorso {
 
     }
 
+    /**
+     * metodo che confronta due beacon se sono uguali
+     * @param beacon1 primo beacon da confrontare
+     * @param beacon2 secondo beacon da confrontare
+     * @return ritorna un booleano con il risultato: True se sono uguali altrimenti false
+     */
     private boolean compare(Beacon beacon1, Beacon beacon2){
 
         boolean uguali = false;
